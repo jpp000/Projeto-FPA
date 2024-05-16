eb35676b9abcf6671674271d7859230f3d3bd663
jpp000 <albuquerquejoaopedro04@gmail.com>
2024-05-16 17:19:56 -0300

    Funcao da criacao da lista implementada com excecoes

--- main.py
+++ main.py
@@ -4,22 +4,35 @@ os.system('cls')
 
 def pega_receita(nome_arquivo):
    receitas = []
-   with open(f'{nome_arquivo}', 'r', encoding='utf-8') as file:
-      next(file)
-      for line in file:
-         nome, pais, *resto = line.strip().split(',', maxsplit=2)
-         
-         if resto[0][0] == '"':
-            resto[0] = resto[0][1:]  
-         ingredientes, preparo = resto[0].split('","')
-         preparo = preparo.strip('"')
+   try:
+      with open(f'{nome_arquivo}', 'r', encoding='utf-8') as file:
+         next(file)
+         for line in file:
+               try:
+                  nome, pais, *resto = line.strip().split(',', maxsplit=2)
+                  
+                  if resto[0][0] == '"':
+                     resto[0] = resto[0][1:]  
+                  ingredientes, preparo = resto[0].split('","')
+                  preparo = preparo.strip('"')
 
-         item = {
-            'nome': nome,
-            'pais': pais.strip(),
-            'ingredientes': ingredientes.strip(),
-            'preparo': preparo
-         }
-         receitas.append(item)
+                  item = {
+                     'nome': nome,
+                     'pais': pais.strip(),
+                     'ingredientes': ingredientes.strip(),
+                     'preparo': preparo
+                  }
+                  receitas.append(item)
+               except ValueError as erro:
+                  print(f"Erro: {erro}.")
+               except Exception as erro:
+                  print(f"Erro: {erro}.")
+   except FileNotFoundError:
+      print(f"Arquivo '{nome_arquivo}' não encontrado.")
+   except Exception as e:
+      print(f"Erro inesperado ao abrir o arquivo '{nome_arquivo}'.")
    
    return receitas
+
+
+receitas = pega_receita('receitas.csv')
